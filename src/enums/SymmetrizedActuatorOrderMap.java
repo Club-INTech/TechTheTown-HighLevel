24c4637d3c9e031d8594f49489e1a101ffd9c8e0
Mardelor <remy.zirnheld@telecom-sudparis>
2017-08-17 15:11:28 +0200

    Scripts & Enums : Gros clean !

--- src/enums/SymmetrizedActuatorOrderMap.java
+++ src/enums/SymmetrizedActuatorOrderMap.java
@@ -40,61 +40,6 @@ public class SymmetrizedActuatorOrderMap
     	// exemple : mCorrespondenceMap.put(ActuatorOrder.CLOSE_DOOR_RIGHT, ActuatorOrder.CLOSE_DOOR_LEFT);
     	// TODO : ajouter les ordres symétriques*
 
-/*			 ____________________
- * 		   *|                    |*
- *		   *|    Pelle T-3000    |*
- *		   *|____________________|*
- */
-
-    //Bras Pelleteuse
-
-        mCorrespondenceMap.put(ActuatorOrder.DEPLOYER_PELLETEUSE_BAS, ActuatorOrder.DEPLOYER_PELLETEUSE_BAS);
-        mCorrespondenceMap.put(ActuatorOrder.LIVRAISON_PELLETEUSE, ActuatorOrder.LIVRAISON_PELLETEUSE);
-        mCorrespondenceMap.put(ActuatorOrder.REPLIER_PELLETEUSE, ActuatorOrder.REPLIER_PELLETEUSE);
-        mCorrespondenceMap.put(ActuatorOrder.MED_PELLETEUSE, ActuatorOrder.MED_PELLETEUSE);
-        mCorrespondenceMap.put(ActuatorOrder.LIVRAISON_PELLETEUSE, ActuatorOrder.LIVRAISON_PELLETEUSE);
-        mCorrespondenceMap.put(ActuatorOrder.DEPLOYER_PELLETEUSE, ActuatorOrder.DEPLOYER_PELLETEUSE);
-
-    //Pelle
-
-        mCorrespondenceMap.put(ActuatorOrder.PRET_PELLE, ActuatorOrder.PRET_PELLE);
-        mCorrespondenceMap.put(ActuatorOrder.P1, ActuatorOrder.P1);
-        mCorrespondenceMap.put(ActuatorOrder.P2, ActuatorOrder.P2);
-        mCorrespondenceMap.put(ActuatorOrder.PREND_PELLE, ActuatorOrder.PREND_PELLE);
-        mCorrespondenceMap.put(ActuatorOrder.LIVRE_PELLE, ActuatorOrder.LIVRE_PELLE);
-        mCorrespondenceMap.put(ActuatorOrder.RANGE_PELLE, ActuatorOrder.RANGE_PELLE);
-        mCorrespondenceMap.put(ActuatorOrder.PELLE_REASSERV, ActuatorOrder.PELLE_REASSERV);
-
-/*			 ___________________
- * 		   *|                   |*
- *		   *|      AM-SSV2      |*
- *		   *|___________________|*
- */
-    //Attrape-Module
-        mCorrespondenceMap.put(ActuatorOrder.REPLI_CALLE_D, ActuatorOrder.REPLI_CALLE_G);
-        mCorrespondenceMap.put(ActuatorOrder.REPLI_CALLE_G, ActuatorOrder.REPLI_CALLE_D);
-        mCorrespondenceMap.put(ActuatorOrder.LIVRE_CALLE_G, ActuatorOrder.LIVRE_CALLE_D);
-        mCorrespondenceMap.put(ActuatorOrder.LIVRE_CALLE_D, ActuatorOrder.LIVRE_CALLE_G);
-
-        mCorrespondenceMap.put(ActuatorOrder.REPOS_CALLE_D, ActuatorOrder.REPOS_CALLE_G);
-        mCorrespondenceMap.put(ActuatorOrder.REPOS_CALLE_G, ActuatorOrder.REPOS_CALLE_D);
-
-
-        mCorrespondenceMap.put(ActuatorOrder.LEVE_ASC, ActuatorOrder.LEVE_ASC);
-        mCorrespondenceMap.put(ActuatorOrder.BAISSE_ASC, ActuatorOrder.BAISSE_ASC);
-
-        mCorrespondenceMap.put(ActuatorOrder.REPOS_LARGUEUR, ActuatorOrder.REPOS_LARGUEUR);
-        mCorrespondenceMap.put(ActuatorOrder.POUSSE_LARGUEUR, ActuatorOrder.POUSSE_LARGUEUR);
-	mCorrespondenceMap.put(ActuatorOrder.POUSSE_LARGUEUR_LENT, ActuatorOrder.POUSSE_LARGUEUR_LENT);
-
-        mCorrespondenceMap.put(ActuatorOrder.REPOS_ATTRAPE_D, ActuatorOrder.REPOS_ATTRAPE_G);
-        mCorrespondenceMap.put(ActuatorOrder.MID_ATTRAPE_D, ActuatorOrder.MID_ATTRAPE_G);
-        mCorrespondenceMap.put(ActuatorOrder.PREND_MODULE_D, ActuatorOrder.PREND_MODULE_G);
-
-        mCorrespondenceMap.put(ActuatorOrder.REPOS_ATTRAPE_G, ActuatorOrder.REPOS_ATTRAPE_D);
-        mCorrespondenceMap.put(ActuatorOrder.MID_ATTRAPE_G, ActuatorOrder.MID_ATTRAPE_D);
-        mCorrespondenceMap.put(ActuatorOrder.PREND_MODULE_G, ActuatorOrder.PREND_MODULE_D);
-
     }
     
     /**
