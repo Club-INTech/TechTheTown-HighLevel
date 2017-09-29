54719c7f07a039fa548bd65baaecb74fc528d21a
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2017-09-30 01:04:16 +0200

    Config : test Vec & Commentaires

--- src/exceptions/Locomotion/PointInObstacleException.java
+++ src/exceptions/Locomotion/PointInObstacleException.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright (c) 2016, INTech.
+ *
+ * This file is part of INTech's HighLevel.
+ *
+ *  INTech's HighLevel is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  INTech's HighLevel is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with it.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package exceptions.Locomotion;
 
 import smartMath.Vec2;
 
 /**
- * Created by shininisan on 08/12/16.
+ * Peut etre utile pour le Pathfinding...
  */
 public class PointInObstacleException extends Exception {
     private Vec2 point;
