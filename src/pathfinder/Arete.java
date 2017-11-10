09b2b7166625ce39c46899cab46fca00336d4a78
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2017-11-10 21:00:45 +0100

    arete

--- src/pathfinder/Arete.java
+++ src/pathfinder/Arete.java
@@ -7,7 +7,7 @@ public class Arete {
     private Noeud noeud2;
 
     private Arete(Noeud noeud1, Noeud noeud2){
-        this.noeud1() = noeud1;
-        this.noeud2() = noeud2;
+        this.noeud1= noeud1;
+        this.noeud2= noeud2;
     }
 }
