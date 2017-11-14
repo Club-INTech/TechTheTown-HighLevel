b4529fc0288f85f87d89920bfc5e6691192cc1d3
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2017-11-14 23:32:17 +0100

    distance de sécurité

--- src/pathfinder/Graphe.java
+++ src/pathfinder/Graphe.java
@@ -58,51 +58,6 @@ public class Graphe {
         }
     }
 
-    /*public void createnoeudTable() {
-        listCircu = obstacleManager.getmCircularObstacle();
-        tabposition.get(0).setX(listCircu.get(0).getPosition().getX());
-        tabposition.get(0).setY(listCircu.get(0).getPosition().getY() + listCircu.get(0).getRadius() + 264);
-        nodes.get(0).position = tabposition.get(0);
-        tabposition.get(1).setX(listCircu.get(1).getPosition().getX() + listCircu.get(1).getRadius() + 264);
-        tabposition.get(1).setY(listCircu.get(1).getPosition().getY());
-        nodes.get(1).position = tabposition.get(1);
-        tabposition.get(2).setX(listCircu.get(1).getPosition().getX() + listCircu.get(1).getRadius() - 264);
-        tabposition.get(2).setY(listCircu.get(1).getPosition().getY());
-        nodes.get(2).position = tabposition.get(2);
-        tabposition.get(3).setX(listCircu.get(1).getPosition().getX());
-        tabposition.get(3).setY(listCircu.get(1).getPosition().getY() + listCircu.get(1).getRadius() - 264);
-        nodes.get(3).position = tabposition.get(3);
-        tabposition.get(4).setX(listCircu.get(2).getPosition().getX() + listCircu.get(2).getRadius() - 264);
-        tabposition.get(4).setY(listCircu.get(2).getPosition().getY());
-        nodes.get(4).position = tabposition.get(4);
-        tabposition.get(5).setX(listCircu.get(2).getPosition().getX());
-        tabposition.get(5).setY(listCircu.get(2).getPosition().getY() + listCircu.get(2).getRadius() - 264);
-        nodes.get(5).position = tabposition.get(5);
-        tabposition.get(6).setX(listCircu.get(3).getPosition().getX());
-        tabposition.get(6).setY(alineate(nodes.get(4).position.getX(), nodes.get(4).position.getY(), listCircu.get(3).getPosition().getX(), listCircu.get(3).getPosition().getY() + 264).getY());
-        nodes.get(6).position = tabposition.get(6);
-        tabposition.get(7).setX(listCircu.get(4).getPosition().getX());
-        tabposition.get(7).setY(listCircu.get(4).getPosition().getY() + listCircu.get(4).getRadius() + 264);
-        nodes.get(7).position = tabposition.get(7);
-        tabposition.get(8).setX(listCircu.get(4).getPosition().getX() + listCircu.get(4).getRadius() + 264);
-        tabposition.get(8).setY(listCircu.get(4).getPosition().getY());
-        nodes.get(8).position = tabposition.get(8);
-        tabposition.get(9).setX(listCircu.get(5).getPosition().getX() + listCircu.get(5).getRadius() + 264);
-        tabposition.get(9).setY(listCircu.get(5).getPosition().getY());
-        nodes.get(9).position = tabposition.get(9);
-        tabposition.get(10).setX(listCircu.get(5).getPosition().getX() + listCircu.get(5).getRadius() - 264);
-        tabposition.get(10).setY(listCircu.get(5).getPosition().getY());
-        nodes.get(10).position = tabposition.get(9);
-        tabposition.get(11).setX(listCircu.get(5).getPosition().getX());
-        tabposition.get(11).setY(alineate(nodes.get(1).position.getX(), nodes.get(1).position.getY(), listCircu.get(5).getPosition().getX(), listCircu.get(3).getPosition().getY() + 264).getY());
-        nodes.get(11).position = tabposition.get(11);
-        tabposition.get(12).setX(alineate(nodes.get(1).position.getX(), nodes.get(1).position.getY(), listCircu.get(6).getPosition().getX(), listCircu.get(6).getPosition().getY() + 264).getX());
-        tabposition.get(12).setY(listCircu.get(6).getPosition().getY());
-        nodes.get(12).position = tabposition.get(12);
-        tabposition.get(13).setX(alineate(nodes.get(4).position.getX(), nodes.get(4).position.getY(), listCircu.get(5).getPosition().getX() - 264, listCircu.get(5).getPosition().getY()).getX());
-        tabposition.get(13).setY(listCircu.get(5).getPosition().getY());
-        nodes.get(13).position = tabposition.get(12);
-    } */
 
     public ArrayList<Noeud> createNodes() {
         int pasX = 300;
@@ -153,10 +108,12 @@ public class Graphe {
                 int rayonObstacleCircu = listCircu.get(j).getRadius();
                 int xNoeud = nodes.get(i).position.getX();
                 int yNoeud = nodes.get(i).position.getY();
+                int distanceDeSecurite=264;
                 if (Math.pow(xNoeud - xcentreObstacleCircu, 2) + Math.pow(yNoeud - ycentreObstacleCircu, 2) < Math.pow(rayonObstacleCircu, 2)) {
                     nodes.remove(i);
                 }
-                if (Math.pow(xNoeud - xcentreObstacleCircu, 2) + Math.pow(yNoeud - ycentreObstacleCircu, 2) < Math.pow(rayonObstacleCircu + 264, 2)) {
+                //test pour savoir si les noeuds se trouvent à côté des obstacles circulaires
+                if (Math.pow(xNoeud - xcentreObstacleCircu, 2) + Math.pow(yNoeud - ycentreObstacleCircu, 2) < Math.pow(rayonObstacleCircu + distanceDeSecurite, 2)) {
                     nodes.get(i).heuristique = 1;
                 }
 
