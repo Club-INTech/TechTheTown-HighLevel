065c710d4b6322a9dbdac03c34033d8b48a823dc
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-01-26 17:20:26 +0100

    DeposeCubes modifiée

--- src/scripts/DeposeCubes.java
+++ src/scripts/DeposeCubes.java
@@ -17,14 +17,12 @@ public class DeposeCubes extends AbstractScript{
 
     public DeposeCubes(Config config, Log log, HookFactory hookFactory){
         super(config, log, hookFactory);
-        /**6  versions pour les 6 blocs de cubes :
-         *  quand on va prendre 4 cubes (pour l'instant 4 le 5 ème à venir ^^) , il faudra déposer la
-         *  tour construite à partir d'un bloc dans la zone de construction, comme on est très forts,
-         *  on va prendre les six, pour les trois premières versions, on ne fait qu'ouvrir la porte
-         *  et reculer, pour les trois autres versions, on se positionne aux mêmes positions d'entrée
-         *  que les trois premières car la zone de construction est petite, il faudra donc pousser
-         *  la tour de cubes, puis ouvrir la porte et finalement git pyreculer*/
-        versions = new Integer[]{0,1,2,3,4,5};
+        /**3 versions de DeposeCubes
+         * La version 0 correspond à l'action de déposer les deux premières tours de cubes
+         * La version 1 correspond aux 2 deuxièmes tours
+         * La version 2 correspond au cas ou on va déposer que la troisième tour mais
+         * pas la quatrième*/
+        versions = new Integer[]{0,1,2};
     }
     @Override
     public void execute(int versionToExecute, GameState stateToConsider) throws ExecuteException, UnableToMoveException {
@@ -36,7 +34,8 @@ public class DeposeCubes extends AbstractScript{
         int d2=20; //c'est la même distance que d dans entryPosition
         int d3=2*l;//mesure à faire pour savoir exactement
         stateToConsider.robot.turn(-Math.PI/2);
-        if (versionToExecute==0) {
+        //la version 0 et 1 corresondent au cas ou on dépose les deux tours
+        if (versionToExecute==0 || versionToExecute==1) {
             stateToConsider.robot.useActuator(ActuatorOrder.OUVRE_LA_PORTE_AVANT,true);
             stateToConsider.robot.moveLengthwise(-d-d2);
             stateToConsider.robot.useActuator(ActuatorOrder.FERME_LA_PORTE_AVANT,true);
@@ -47,8 +46,11 @@ public class DeposeCubes extends AbstractScript{
             stateToConsider.robot.moveLengthwise(-d-d2);
             stateToConsider.robot.useActuator(ActuatorOrder.FERME_LA_PORTE_ARRIERE,true);
         }
-        if(versionToExecute==1 || versionToExecute==2){
-
+        //on dépose la troisième tour au cas ou on arrive pas à prendre la 4ème
+        if(versionToExecute==3){
+            stateToConsider.robot.useActuator(ActuatorOrder.OUVRE_LA_PORTE_AVANT,true);
+            stateToConsider.robot.moveLengthwise(-d-d2);
+            stateToConsider.robot.useActuator(ActuatorOrder.FERME_LA_PORTE_AVANT,true);
         }
         /*il s'agit des cubes pris de la zone adverse, comme la zone de construction est assez petite, on
         devrait plutot pousser les tours déjà faites
