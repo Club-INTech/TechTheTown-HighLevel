2e69c4f329e997467b8ecb91a128131d8cee8d13
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-02-01 11:35:11 +0100

    construction de l'arbre de l'IA

--- src/strategie/IA/IA.java
+++ src/strategie/IA/IA.java
@@ -10,6 +10,7 @@ import exceptions.Locomotion.UnableToMoveException;
 import hook.HookFactory;
 import org.classpath.icedtea.Config;
 import scripts.ActivationBrasLateral;
+import scripts.DeposeCubes;
 import scripts.TakeCubes;
 import strategie.GameState;
 import utils.Log;
@@ -44,13 +45,54 @@ public class IA implements Service {
     public void create() {
         ArrayList<Node> lnode1 = new ArrayList<Node>();
         //noeud du pattern
-        Node node1=new Node("patern", root, 666, 5, null,0,
+        Node node1=new Node("pattern", root, 666, 0, null,0,gameState,null);
         lnode1.add(node1);
         root.setNextNodes(lnode1);
         ArrayList<Node> lnode2=new ArrayList<>();
         //noeud interrupteur
-        Node node2=new Node("interrupteur",node1,666,25,new ActivationBrasLateral(container.getConfig(),container.getLog(),hookFactory),0,null,gameState);
-
+        Node node2=new Node("interrupteur",node1,666,25,new ActivationBrasLateral(container.getConfig(),container.getLog(),hookFactory),0,gameState,null);
+        lnode2.add(node2);
+        node1.setNextNodes(lnode2);
+        ArrayList<Node> lnode3=new ArrayList<>();
+        //noeud des cubes de la position 2
+        Node node3=new Node("Cubes 2",node2,666,0,new TakeCubes(container.getConfig(),container.getLog(),hookFactory),2,null,gameState);
+        lnode3.add(node3);
+        node2.setNextNodes(lnode3);
+        ArrayList<Node> lnodes4=new ArrayList<>();
+        //noeud de l'abeille
+        Node node4=new Node("abeille",node3,666,50,new ActivationBrasLateral(container.getConfig(),container.getLog(),hookFactory),1,gameState,null);
+        lnodes4.add(node4);
+        node3.setNextNodes(lnodes4);
+        ArrayList<Node> lnode5=new ArrayList<>();
+        //noeud des cubes de la position 0
+        Node node5=new Node("Cubes 2",node4,666,0,new TakeCubes(container.getConfig(),container.getLog(),hookFactory),0,null,gameState);
+        lnode5.add(node5);
+        node4.setNextNodes(lnode5);
+        ArrayList<Node> lnodes6=new ArrayList<>();
+        //noeud pour déposer tous les cubes
+        Node node6=new Node("deposer les cubes 2 et 0",node5,666,80,new DeposeCubes(container.getConfig(),container.getLog(),hookFactory),0,gameState,null);
+        lnodes6.add(node6);
+        node5.setNextNodes(lnodes6);
+        ArrayList<Node> lnode7=new ArrayList<>();
+        //noeud pour prendre les cubes de la position 1
+        Node node7=new Node("Cubes 1",node6,666,0,new TakeCubes(container.getConfig(),container.getLog(),hookFactory),0,null,gameState);
+        lnode7.add(node7);
+        node6.setNextNodes(lnode7);
+        ArrayList<Node> lnodes8=new ArrayList<>();
+        //noeud pour prendre les cubes qui restent de la position 1
+        Node node8=new Node("Cubes qui restent position 1",node7,666,0,new TakeCubes(container.getConfig(),container.getLog(),hookFactory),1,null,gameState);
+        lnodes8.add(node8);
+        node7.setNextNodes(lnodes8);
+        ArrayList<Node> lnode9=new ArrayList<>();
+        //noeud pour prendre les cubes qui restent de la position 0
+        Node node9=new Node("Cubes qui restent position 0",node8,666,0,new TakeCubes(container.getConfig(),container.getLog(),hookFactory),0,null,gameState);
+        lnode9.add(node9);
+        node8.setNextNodes(lnode9);
+        ArrayList<Node> lnode10=new ArrayList<>();
+        //déposer tous les cubes qui restent
+        Node node10=new Node("déposer tous les cubes",node9,666,46,new DeposeCubes(container.getConfig(),container.getLog(),hookFactory),1,gameState,null);
+        lnode10.add(node10);
+        node9.setNextNodes(lnode10);
     }
 
     //parcourt l'arbre si il y a une exception
