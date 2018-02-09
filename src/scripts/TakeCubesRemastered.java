2318ac434b73abfe226c8a22779dbc28b2f0496e
Nayht <william.huver@teleco-sudparis.eu>
2018-02-09 18:09:53 +0100

    Ajout de commentaires pour le script TakeCubesRemastered.java

--- src/scripts/TakeCubesRemastered.java
+++ src/scripts/TakeCubesRemastered.java
@@ -14,7 +14,8 @@ import strategie.GameState;
 import tests.container.A;
 import utils.Log;
 
-
+/** Script permettant de récupérer les cubes de n'importe quel tas, selon n'importe quel pattern, dans n'importe quelle direction
+ */
 public class TakeCubesRemastered extends AbstractScript {
     private int largeurCubes;
     private int longueurBras;
@@ -22,36 +23,55 @@ public class TakeCubesRemastered extends AbstractScript {
 
     public TakeCubesRemastered(Config config, Log log, HookFactory hookFactory) {
         super(config, log, hookFactory);
-        this.largeurCubes=config.getInt(ConfigInfoRobot.LONGUEUR_CUBE);
-        this.longueurBras=config.getInt(ConfigInfoRobot.LONGUEUR_BRAS);
+        this.updateConfig();
     }
 
+    /** Execution du script de récupération des cubes
+     * @param indicePattern
+     * @param tas
+     * @param bras
+     * @param additionalCube
+     * @param stateToConsider
+     * @throws InterruptedException
+     * @throws ExecuteException
+     * @throws UnableToMoveException
+     * @throws PatternNotYetCalculatedException
+     * @throws PatternNotRecognizedException
+     */
     public void execute(int indicePattern, TasCubes tas, BrasUtilise bras, Cubes additionalCube, GameState stateToConsider)
             throws InterruptedException, ExecuteException, UnableToMoveException, PatternNotYetCalculatedException, PatternNotRecognizedException{
         stateToConsider.robot.useActuator(ActuatorOrder.FERME_LA_PORTE_AVANT, true);
+        //Si indicePattern==-2, c'est que le pattern n'a pas encore été calculé
         if (indicePattern != -2){
+            //Si indicePattern==-1, c'est que le pattern n'a pas pu être identifié
             if (indicePattern != -1) {
                 int[][] successivesPositionsList;
+                //Si additionalCube.getColor()==Colors.NULL, c'est qu'on a choisi de ne prendre que 3 cubes
+                //Sinon, la couleur de additionalCube sera correspondra au cube qui sera pris après le pattern
                 if (additionalCube.getColor() == Colors.NULL) {
                     successivesPositionsList = new int[3][2];
                 } else {
                     successivesPositionsList = new int[4][2];
+                    //On calcule les positions du cube additionnel pour x et y :
+                    // position = position du tas + position relative du cube choisi par rapport au tas
                     successivesPositionsList[3][0] = tas.getCoords()[0] + additionalCube.getRelativeCoords()[0] * largeurCubes;
                     successivesPositionsList[3][1] = tas.getCoords()[1] + additionalCube.getRelativeCoords()[1] * largeurCubes;
                 }
 
+                //On récupère les couleurs composant le pattern reconnu (le pattern reconnu est identifié grâce à indicePattern)
                 Colors[] pattern = Patterns.getPatternFromID(indicePattern);
                 for (int i = 0; i < 3; i++) {
+                    //On calcule les positions des cubes pour x et y :
+                    // position = position du tas + position relative du cube choisi par rapport au tas
                     successivesPositionsList[i][0] = tas.getCoords()[0] + Cubes.findRelativeCoordsWithColor(pattern[i])[0] * largeurCubes;
                     successivesPositionsList[i][1] = tas.getCoords()[1] + Cubes.findRelativeCoordsWithColor(pattern[i])[1] * largeurCubes;
                 }
 
+                //On définit les Vec2 correspondant aux positions où le robot doit aller pour prendre les cubes
                 Vec2 firstPosition = new Vec2(successivesPositionsList[0][0], successivesPositionsList[0][1]);
                 Vec2 secondPosition = new Vec2(successivesPositionsList[1][0], successivesPositionsList[1][1]);
                 Vec2 thirdPosition = new Vec2(successivesPositionsList[2][0], successivesPositionsList[2][1]);
-                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_ELECTROVANNE_AVANT,true);
-                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_ELECTROVANNE_ARRIERE,true);
-                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_LA_POMPE, true);
+
                 if (bras==BrasUtilise.ARRIERE){
                     direction="backward";
                     stateToConsider.robot.turnRelatively(Math.PI);
@@ -59,15 +79,34 @@ public class TakeCubesRemastered extends AbstractScript {
                 else{
                     direction="forward";
                 }
+                //On active la pompe, et ouvre les électrovannes
+                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_ELECTROVANNE_AVANT,true);
+                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_ELECTROVANNE_ARRIERE,true);
+                stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_LA_POMPE, true);
+
+                //On fait aller le robot à la position pour prendre le premier cube du pattern
                 stateToConsider.robot.moveNearPoint(firstPosition, longueurBras, direction);
+                //Le robot execute les actions pour prendre le cube
                 takethiscube(stateToConsider, bras.getSide());
+
+                //On fait aller le robot à la position pour prendre le deuxième cube du pattern
                 stateToConsider.robot.moveNearPoint(secondPosition, longueurBras, direction);
+                //Le robot execute les actions pour prendre le cube
                 takethiscube(stateToConsider, bras.getSide());
+
+                //On fait aller le robot à la position pour prendre le troisième cube du pattern
                 stateToConsider.robot.moveNearPoint(thirdPosition, longueurBras, direction);
+                //Le robot execute les actions pour prendre le cube
                 takethiscube(stateToConsider, bras.getSide());
+
+                //Si un cube additionnel a été précisé
                 if (additionalCube.getColor() != Colors.NULL){
+                    //On définit le Vec2 de la position permettant de prendre le cube additionnel
                     Vec2 fourthPosition = new Vec2(successivesPositionsList[3][0], successivesPositionsList[3][1]);
+
+                    //On fait aller le robot à la position pour prendre le cube additionnel.
                     stateToConsider.robot.moveNearPoint(fourthPosition, longueurBras, direction);
+                    //Le robot execute les actions pour prendre le cube
                     takethiscube(stateToConsider, bras.getSide());
                 }
                 stateToConsider.robot.useActuator(ActuatorOrder.DESACTIVE_LA_POMPE, true);
@@ -86,6 +125,7 @@ public class TakeCubesRemastered extends AbstractScript {
 
 
     public void takethiscube(GameState stateToConsider, String bras) throws InterruptedException{
+        //Vazy wesh si t'as besoin d'explications pour ça c'est que tu sais pas lire
         if (bras.equals("avant")) {
             stateToConsider.robot.useActuator(ActuatorOrder.ACTIVE_ELECTROVANNE_AVANT,true);
             stateToConsider.robot.useActuator(ActuatorOrder.DESACTIVE_ELECTROVANNE_ARRIERE, true);
@@ -103,30 +143,44 @@ public class TakeCubesRemastered extends AbstractScript {
 
     }
 
+    @Override
     public void execute(int placeHolder, GameState placeHolder2){
 
     }
 
+    @Override
     public Circle entryPosition(int version, int rayon, Vec2 robotPosition){
         return new Circle(robotPosition,0);
     }
 
+    @Override
     public void finalize(GameState state, Exception e) throws UnableToMoveException {
 
     }
 
+    @Override
     public int remainingScoreOfVersion(int version, final GameState state) {
         return 0;
     }
 
+    @Override
     public Integer[] getVersion(GameState stateToConsider) {
         return new Integer[]{};
     }
 
+    @Override
     public Integer[][] getVersion2(GameState stateToConsider) {
         return versions2;
     }
 
+    @Override
     public void execute(GameState stateToConsider,Boolean pousse) throws InterruptedException, UnableToMoveException, ExecuteException, BlockedActuatorException{
+    }
+
+    @Override
+    public void updateConfig() {
+        super.updateConfig();
+        this.largeurCubes=config.getInt(ConfigInfoRobot.LONGUEUR_CUBE);
+        this.longueurBras=config.getInt(ConfigInfoRobot.LONGUEUR_BRAS);
     }
 }
