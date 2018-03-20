0171530c68d393043bd0e9b2b2ee7654e51f9f05
Sam <samuel.rac@telecom-sudparis.eu>
2018-03-20 18:58:41 +0100

    modification position d'entrée

--- src/table/Table.java
+++ src/table/Table.java
@@ -66,7 +66,7 @@ public class Table implements Service
 	// Au besoin, créer les classes nécessaires dans le package table
 
 	/** point de départ du match à modifier a chaque base roulante */
-	public static Vec2 entryPosition = new Vec2(1269, 455);
+	public static Vec2 entryPosition = new Vec2(1300, 430);
 	public static double entryOrientation = Math.PI;
 
 	/**
