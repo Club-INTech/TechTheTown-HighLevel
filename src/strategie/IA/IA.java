652c9ad8da935f89290151f00e4ddcfaa8ceff34
Sam <samuel.rac@telecom-sudparis.eu>
2018-03-24 20:34:26 +0100

    IA plus propre

--- src/strategie/IA/IA.java
+++ src/strategie/IA/IA.java
@@ -17,108 +17,33 @@ import strategie.GameState;
 import sun.font.Script;
 import utils.Log;
 
+import java.security.spec.ECField;
 import java.util.ArrayList;
 
 public class IA implements Service {
 
-    private Node root;
-    private GameState gameState;
-    private int scorefinal;
-    private ScriptManager scriptManager;
-    private HookFactory hookFactory;
+    Node root;
+    GameState gameState;
+    ScriptManager scriptManager;
 
-    public IA(GameState gameState, ScriptManager scriptManager, HookFactory hookFactory) throws InterruptedException, ContainerException {
-        this.root = new Node("root", null, 666, 0, null, 0, null, gameState);
-        this.root.setExecuted(true);
+    public IA(GameState gameState, ScriptManager scriptManager) {
+        root = new Node(null,0,0,null,gameState);
         this.gameState = gameState;
-        this.scorefinal = 0;
         this.scriptManager = scriptManager;
-        this.hookFactory = hookFactory;
     }
+
+    public void create()  {
+
+    }
+
+    public void execute(Exception e) throws BlockedActuatorException, UnableToMoveException, PointInObstacleException, ExecuteException, BadVersionException {
+        Node node = root.selectNode();
+        node.execute(e);
+    }
+
 
     @Override
     public void updateConfig() {
-
-    }
-
-
-    //génère l'arbre
-    public void create() throws InterruptedException, ContainerException {
-        ArrayList<Node> lnode1 = new ArrayList<Node>();
-        //noeud du pattern
-        Node node1 = new Node("pattern", root, 666, 0, null, 0, null, gameState);
-        lnode1.add(node1);
-        root.setNextNodes(lnode1);
-        ArrayList<Node> lnode2 = new ArrayList<>();
-        //noeud interrupteur
-        Node node2 = new Node("interrupteur", node1, 666, 25, scriptManager.getScript(ScriptNames.ACTIVATION_PANNEAU_DOMOTIQUE), 0, null, gameState);
-        lnode2.add(node2);
-        node1.setNextNodes(lnode2);
-        ArrayList<Node> lnode3 = new ArrayList<>();
-        //noeud des cubes de la position 2
-        Node node3 = new Node("Cubes 2", node2, 666, 0, scriptManager.getScript(ScriptNames.TAKE_CUBES), 2, null, gameState);
-        lnode3.add(node3);
-        node2.setNextNodes(lnode3);
-        ArrayList<Node> lnodes4 = new ArrayList<>();
-        //noeud de l'abeille
-        Node node4 = new Node("abeille", node3, 666, 50,scriptManager.getScript(ScriptNames.ACTIVE_ABEILLE), 0, null,gameState);
-        lnodes4.add(node4);
-        node3.setNextNodes(lnodes4);
-        ArrayList<Node> lnode5 = new ArrayList<>();
-        //noeud des cubes de la position 0
-        Node node5 = new Node("Cubes 0", node4, 666, 0, scriptManager.getScript(ScriptNames.TAKE_CUBES), 0, null, gameState);
-        lnode5.add(node5);
-        node4.setNextNodes(lnode5);
-        ArrayList<Node> lnodes6 = new ArrayList<>();
-        //noeud pour déposer tous les cubesc
-        Node node6 = new Node("deposer les cubes 2 et 0", node5, 666, 80, scriptManager.getScript(ScriptNames.DEPOSE_CUBES), 0,  null,gameState);
-        lnodes6.add(node6);
-        node5.setNextNodes(lnodes6);
-        ArrayList<Node> lnode7 = new ArrayList<>();
-        //noeud pour prendre les cubes de la position 1
-        Node node7 = new Node("Cubes 1", node6, 666, 0, scriptManager.getScript(ScriptNames.TAKE_CUBES), 0, null, gameState);
-        lnode7.add(node7);
-        node6.setNextNodes(lnode7);
-        ArrayList<Node> lnodes8 = new ArrayList<>();
-        //noeud pour déposer les cubes qui restent de la position 1
-        Node node8 = new Node("Cubes qui restent position 1", node7, 666, 0,scriptManager.getScript(ScriptNames.DEPOSE_CUBES), 1, null, gameState);
-        lnodes8.add(node8);
-        node7.setNextNodes(lnodes8);
-    }
-
-    //parcourt l'arbre si il y a une exception
-    public void execute(GameState gs, Exception e) throws UnableToMoveException, ExecuteException, BlockedActuatorException, PointInObstacleException, BadVersionException {
-        root.execute(gs, e);
-    }
-
-
-    public Node getRoot() {
-        return root;
-    }
-
-    public GameState getGameState() {
-        return gameState;
-    }
-
-    /**
-     *Il s'agit d'une méthode récursive pour calculer le score, on lui file comme paramètre en Node
-     * root
-     */
-
-    public int getscorefinal(Exception e,Node node) {
-        //on get la dernière version de l'arbre
-       root.updateConditions(e);
-       //on get les fils du noeud passé en paramètre
-       ArrayList<Node> lnodes=node.getNextNodes();
-       for(Node noeud : lnodes){
-           //si l'un des noeuds fils est executé, on rajoute son score au score final
-           if(noeud.getExecuted()){
-               scorefinal+=noeud.getscore();
-               //on refait la même chose avec le noeud executé
-               return getscorefinal(e,noeud);
-           }
-       }
-       return scorefinal;
 
     }
 
--- src/strategie/IA/Node.java
+++ src/strategie/IA/Node.java
@@ -1,5 +1,6 @@
 package strategie.IA;
 
+import enums.ScriptNames;
 import exceptions.BadVersionException;
 import exceptions.BlockedActuatorException;
 import exceptions.ExecuteException;
@@ -7,138 +8,75 @@ import exceptions.Locomotion.PointInObstacleException;
 import exceptions.Locomotion.UnableToMoveException;
 import pathfinder.Noeud;
 import scripts.AbstractScript;
+import scripts.ScriptManager;
 import strategie.GameState;
 
 import java.util.ArrayList;
 
 public class Node {
 
-    private Node previous;
-    private ArrayList<Node> nextNodes;
-    private Boolean condition;  //si true on execute ce noeud
-    private Boolean executed;   //si true execute n'a soulevé aucune exeption
-    private String action;      //nom de l'action réalisée
-    private double time;           //ne pas executer le script si le match a duré plus longtemps que cette valeur
-    private int score;
+    AbstractScript script;
+    int versionToExecute;
+    int score;
+    int timeLimit;
+    int timeToGo;
+    int timeToExexecute;
+    boolean isDone;
+    ArrayList<Node> nextNodes;
+    GameState gameState;
 
-    private GameState gamestate;
-    private AbstractScript script;
-    private Exception exception;
-    private int versionToexecute;
-
-
-
-    public Node(String action, Node previous, long time, int score, AbstractScript script,int versionToexecute ,Exception exception, GameState gamestate) {
-        this.previous = previous;
-        this.nextNodes = null;
-        this.condition = false;
-        this.executed = false;
-        this.action = action;
-        this.time = time;
-        this.gamestate = gamestate;
+    public Node(AbstractScript script, int versionToExecute, int score, ArrayList<Node> nextNodes, GameState gameState) {
         this.script = script;
-        this.exception = exception;
+        this.versionToExecute = versionToExecute;
         this.score = score;
-        this.versionToexecute=versionToexecute;
+        this.timeLimit = 0;
+        this.timeToGo = 0;
+        this.timeToExexecute = 0;
+        this.isDone = false;
+        this.nextNodes = nextNodes;
+        this.gameState = gameState;
     }
 
-
-    /**met à jour la condition pour savoir si ce noeud doit être executé
-    si aucune exception n'est levée, le noeud doit necessairement etre executée     */
-
-    public void updateCondition(Exception e) {
-        if (this.exception == null && this.previous.getExecuted()) {
-            condition = true;
+    public void execute(Exception e) throws BlockedActuatorException, UnableToMoveException, PointInObstacleException, ExecuteException, BadVersionException {
+        if (e != null) {
+            //gestion des exeptions
         } else {
-            condition = false;
-//            return gamestate.getTimeEllapsed() < time && gamestate.robot.getScriptDone().get(script) && !exception.equals(null) || exception == e && this.previous.executed;
-        }
-
-    }
-
-    /** met à jour la condition de tout les noeuds enfants */
-
-    public void updateConditions(Exception e) {
-        for (Node node : nextNodes) {
-            node.updateCondition(e);
+            script.goToThenExec(versionToExecute, gameState);
+            setDone(true);
         }
     }
 
-    public Node getNextNode(Exception e){
-        for(Node node : nextNodes){
-            node.updateCondition(e);
-            if(node.getCondition() && node.getExecuted()){
-                return node.getNextNode(e);
-            }
-            if (node.getCondition()){
-                return node;
+    public Node selectNode() {
+        int bestScore = 0;
+        int i = 0;
+        Node currentNode;
+        for (int j = 0; j < nextNodes.size(); j++) {
+            currentNode = nextNodes.get(j);
+            if (currentNode.getScore() > bestScore) {
+                bestScore = currentNode.getScore();
+                i = j;
             }
         }
-        return null;
+        return nextNodes.get(i);
     }
 
-    /** Cette méthode retourne le noeud executé parmi les noeuds fils du node pris en
-      paramètre c'est pour l'IA     */
-
-    public Node returnExecutedNode(Node node) {
-        for (Node noeud : node.getNextNodes()) {
-            if (noeud.getExecuted()) {
-                return noeud;
-            }
-        }
-        return null;
+    public boolean isDone() {
+        return isDone;
     }
-
-    /** lance l'action du noeud */
-
-    public void execute(GameState gs, Exception e) throws UnableToMoveException, ExecuteException, BlockedActuatorException, PointInObstacleException, BadVersionException {
-        Node node = getNextNode(e);
-        node.script.goToThenExec(this.versionToexecute, gs);
-        node.executed = true;
-    }
-
-    /** Affiche les noeuds suivants */
-
-    public void display() {
-        System.out.println(this);
-        if (nextNodes != null) {
-            for (Node node : nextNodes) {
-                node.display();
-            }
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "action=" + action;
-    }
-
 
     public ArrayList<Node> getNextNodes() {
         return nextNodes;
     }
 
-    public Node getPrevious() {
-        return previous;
+    public int getScore() {
+        return score;
     }
 
-    public Boolean getCondition() {
-        return condition;
-    }
-
-    public Boolean getExecuted() {
-        return executed;
-    }
-
-    public int getscore() {
-        return this.score;
+    public void setDone(boolean done) {
+        isDone = done;
     }
 
     public void setNextNodes(ArrayList<Node> nextNodes) {
         this.nextNodes = nextNodes;
-    }
-
-    public void setExecuted(Boolean executed) {
-        this.executed = executed;
     }
 }
