0210626b3c0b034c19c37c3d092a91dceba7318a
Sam <samuel.rac@telecom-sudparis.eu>
2018-04-19 12:57:29 +0200

    Erreur import de lib

--- src/strategie/IA/DeposeCubes.java
+++ src/strategie/IA/DeposeCubes.java
@@ -4,8 +4,8 @@ import enums.ScriptNames;
 import exceptions.BadVersionException;
 import exceptions.Locomotion.UnableToMoveException;
 import hook.HookFactory;
-import org.classpath.icedtea.Config;
 import pathfinder.Pathfinding;
+import pfg.config.Config;
 import scripts.ScriptManager;
 import smartMath.Vec2;
 import strategie.GameState;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class DeposeCubes extends Node {
 
-    public DeposeCubes(String name, int versionToExecute, ArrayList<Node> nextNodes, ScriptManager scriptManager, GameState gameState, Pathfinding pathfinding, HookFactory hookFactory, pfg.config.Config config, Log log) throws BadVersionException {
+    public DeposeCubes(String name, int versionToExecute, ArrayList<Node> nextNodes, ScriptManager scriptManager, GameState gameState, Pathfinding pathfinding, HookFactory hookFactory, Config config, Log log) throws BadVersionException {
         super(name, versionToExecute, nextNodes, scriptManager, gameState ,pathfinding,hookFactory,config, log);
         this.setScript(scriptManager.getScript(ScriptNames.DEPOSE_CUBES));
         this.setScore(0);
