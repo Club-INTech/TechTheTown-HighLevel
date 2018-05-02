9bf4572430ab35f9c30468124cf9d7a89af832c4
Nayht <william.huver@teleco-sudparis.eu>
2018-05-02 21:34:29 +0200

    On set la vitesse par défaut dans le Junit_Deplacements

--- src/tests/JUnit_Deplacements.java
+++ src/tests/JUnit_Deplacements.java
@@ -1,5 +1,6 @@
 package tests;
 
+import enums.Speed;
 import exceptions.Locomotion.ImmobileEnnemyForOneSecondAtLeast;
 import exceptions.Locomotion.UnableToMoveException;
 import org.junit.Before;
@@ -19,6 +20,7 @@ public class JUnit_Deplacements extends JUnit_Test {
             robot = container.getService(Robot.class);
             state = container.getService(GameState.class);
             container.startInstanciedThreads();
+            robot.setLocomotionSpeed(Speed.DEFAULT_SPEED);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,8 +43,8 @@ public class JUnit_Deplacements extends JUnit_Test {
     public void testRotation() {
         try {
             for (int i=0; i<100; i++){
-                robot.turn(Math.PI/2);
-                robot.turn(-Math.PI/2);
+                robot.turn(4*Math.PI/10);
+                robot.turn(-4*Math.PI/10);
             }
         } catch (UnableToMoveException e) {
             e.printStackTrace();
