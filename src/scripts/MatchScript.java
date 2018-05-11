225dcef7fa4bf91d7b2f60dbf93226742c52934c
Sam <samuel.rac@telecom-sudparis.eu>
2018-05-11 18:32:00 +0200

    Ajout du MatchScript 42

--- src/scripts/MatchScript.java
+++ src/scripts/MatchScript.java
@@ -15,6 +15,7 @@ import pfg.config.Config;
 import smartMath.Circle;
 import smartMath.Vec2;
 import strategie.GameState;
+import strategie.IA.Panneau;
 import utils.Log;
 
 public class MatchScript extends AbstractScript {
@@ -24,7 +25,7 @@ public class MatchScript extends AbstractScript {
 
     public MatchScript(Config config, Log log, HookFactory hookFactory){
         super(config,log,hookFactory);
-        versions = new int[]{0,1,2,3,99};
+        versions = new int[]{0,1,2,3,42};
         updateConfig();
     }
 
@@ -156,35 +157,6 @@ public class MatchScript extends AbstractScript {
 
         }
 
-        if (version==99){
-            //Pile cube n°1 et n°2
-            TakeCubes tk12 = new TakeCubes(config,log,hookFactory);
-            tk12.goToThenExec(120,gameState);
-            //Pile cube n°1 et n°2
-
-            //On active l'abeille
-            ActiveAbeille activeAbeille=new ActiveAbeille(config,log,hookFactory);
-            activeAbeille.goToThenExec(0,gameState);
-
-            //On dépose les cubes à la première position
-            DeposeCubes dpCubes0 = new DeposeCubes(config, log, hookFactory);
-            dpCubes0.goToThenExec(0, gameState);
-
-            //Pile cube n°0
-            TakeCubes tk0 = new TakeCubes(config,log,hookFactory);
-            tk0.goToThenExec(0,gameState);
-            //Pile cube n°0
-
-            //Interrupteur
-            ActivationPanneauDomotique actPD=new ActivationPanneauDomotique(config,log,hookFactory);
-            actPD.goToThenExec(0,gameState);
-            //Interrupteur
-
-            //On dépose les cubes à la deuxième position
-            DeposeCubes dpCubes1=new DeposeCubes(config,log,hookFactory);
-            dpCubes1.goToThenExec(1, gameState);
-
-        }
         if(version==3){
             if (usingAdvancedDetection) {
                 gameState.robot.useActuator(ActuatorOrder.SUS_OFF,true);
@@ -219,11 +191,41 @@ public class MatchScript extends AbstractScript {
 
             //On prend le tas de cubes 2
             TakeCubes tk2=new TakeCubes(config,log,hookFactory);
-            tk2.goToThenExec(2,gameState);
+            tk2.goToThenExec(2, gameState);
 
             //On dépose les cubes à la deuxième position
             DeposeCubes dpCubes1=new DeposeCubes(config,log,hookFactory);
             dpCubes1.goToThenExec(1, gameState);
+        }
+
+        // Script de la victoire !
+        if (version == 42) {
+
+            TakeCubes takeCubes2 = new TakeCubes(config, log, hookFactory);
+            takeCubes2.goToThenExec(2,gameState);
+
+            TakeCubes takeCubes0 = new TakeCubes(config, log, hookFactory);
+            takeCubes0.goToThenExec(0, gameState);
+
+            ActivationPanneauDomotique activationPanneauDomotique = new ActivationPanneauDomotique(config, log, hookFactory);
+            activationPanneauDomotique.goToThenExec(0,gameState);
+
+            DeposeCubes deposeCubes2 = new DeposeCubes(config, log, hookFactory);
+            deposeCubes2.goToThenExec(2,gameState);
+
+            TakeCubes takeCubes1 = new TakeCubes(config, log, hookFactory);
+            takeCubes1.goToThenExec(1, gameState);
+
+            DeposeCubes deposeCubes0 = new DeposeCubes(config, log, hookFactory);
+            deposeCubes0.goToThenExec(0, gameState);
+
+            //
+
+        ActiveAbeille activeAbeille1 = new ActiveAbeille(config, log, hookFactory);
+
+
+
+
         }
         log.debug("////////// End MatchScript version "+version+" //////////");
     }
