77cfff8f3d6580ba1541d31328e3503a301603c9
Nayht <william.huver@teleco-sudparis.eu>
2018-05-11 22:39:13 +0200

    if not isinstance(frame, NoneType)

--- src/patternRecognition/setPatternPosition/SetPatternPositionGreen.py
+++ src/patternRecognition/setPatternPosition/SetPatternPositionGreen.py
@@ -112,85 +112,86 @@ colorValue3=0
 while(True):
     ##### CAPTURE DE LA VIDEO FRAME PAR FRAME
     ret, frame = cap.read()
-    frame=frame[YSTART:YSTART+HAUTEUR_DISPLAY,XSTART:XSTART+LARGEUR_DISPLAY]
-    cv2.namedWindow('frame')
-    cv2.setMouseCallback('frame', setCalquePosition)
+    if not isinstance(frame, NoneType)
+        frame=frame[YSTART:YSTART+HAUTEUR_DISPLAY,XSTART:XSTART+LARGEUR_DISPLAY]
+        cv2.namedWindow('frame')
+        cv2.setMouseCallback('frame', setCalquePosition)
 
-    ##### TRAITEMENT DE L'IMAGE
-    hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        ##### TRAITEMENT DE L'IMAGE
+        hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
 
-    #Change la saturation
-    hsv[:,:,1]=np.clip(np.around(hsv[:,:,1]*saturationMultiplier,1),0,255)
-    #Change la luminosite
-    hsv[:,:,2]=np.clip(np.around(hsv[:,:,2]*brightnessMultiplier,1),0,255)
+        #Change la saturation
+        hsv[:,:,1]=np.clip(np.around(hsv[:,:,1]*saturationMultiplier,1),0,255)
+        #Change la luminosite
+        hsv[:,:,2]=np.clip(np.around(hsv[:,:,2]*brightnessMultiplier,1),0,255)
 
-    img=cv2.cvtColor(hsv,cv2.COLOR_HSV2RGB)
+        img=cv2.cvtColor(hsv,cv2.COLOR_HSV2RGB)
 
-    ##### On sort de la boucle ici
-    if toBreak:
-        #SAUVEGARDER L'IMAGE ICI
-        cv2.imwrite("/tmp/ImageRaspi.jpeg",img)
-        #SAUVEGARDER LES POSITIONS DES CALQUES ICI
-        file = open("/tmp/CoordsPatternVideo.txt","w")
-        file.write(str(xCalque1+XSTART)+" "+str(yCalque1+YSTART)+" "+str(xCalque2+XSTART)+" "+str(yCalque2+YSTART)+" "+str(xCalque3+XSTART)+" "+str(yCalque3+YSTART))
-        file.close()
-        break
+        ##### On sort de la boucle ici
+        if toBreak:
+            #SAUVEGARDER L'IMAGE ICI
+            cv2.imwrite("/tmp/ImageRaspi.jpeg",img)
+            #SAUVEGARDER LES POSITIONS DES CALQUES ICI
+            file = open("/tmp/CoordsPatternVideo.txt","w")
+            file.write(str(xCalque1+XSTART)+" "+str(yCalque1+YSTART)+" "+str(xCalque2+XSTART)+" "+str(yCalque2+YSTART)+" "+str(xCalque3+XSTART)+" "+str(yCalque3+YSTART))
+            file.close()
+            break
 
-    ##### AJOUT DES ANNOTATIONS SUR L'IMAGE
-    #Assure le clignotement de l'image
-    i+=1
-    if i==20:
-        tmp=colorValue3
-        colorValue3=colorValue2
-        colorValue2=colorValue1
-        colorValue1=tmp
-        i=0
+        ##### AJOUT DES ANNOTATIONS SUR L'IMAGE
+        #Assure le clignotement de l'image
+        i+=1
+        if i==20:
+            tmp=colorValue3
+            colorValue3=colorValue2
+            colorValue2=colorValue1
+            colorValue1=tmp
+            i=0
 
-    #On met les cercles et les numeros
-    cv2.circle(img, (xCalque1,yCalque1),5,(colorValue1,colorValue2,colorValue3),1)
-    cv2.putText(img,'1',(xCalque1-8,yCalque1-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
-    cv2.circle(img, (xCalque2,yCalque2),5,(colorValue1,colorValue2,colorValue3),1)
-    cv2.putText(img,'2',(xCalque2-8,yCalque2-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
-    cv2.circle(img, (xCalque3,yCalque3),5,(colorValue1,colorValue2,colorValue3),1)
-    cv2.putText(img,'3',(xCalque3-8,yCalque3-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
-
-
-    ##### KEYBOARD LISTENER
-    #On ecoute les touches appuyees
-    key = cv2.waitKey(1) & 0xFF
-    if key==ord("0") or key==ord(" "):
-        currentCalque=0
-    elif key==ord("1"):
-        currentCalque=1
-    elif key==ord("2"):
-        currentCalque=2
-    elif key==ord("3"):
-        currentCalque=3
-
-    #LEFT
-    elif key==ord('q'):
-        moveCalqueLeft()
-
-    #UP
-    elif key==ord('z'):
-        moveCalqueUp()
-
-    #RIGHT
-    elif key==ord('d'):
-        moveCalqueRight()
-
-    #DOWN
-    elif key==ord('s'):
-        moveCalqueDown()
+        #On met les cercles et les numeros
+        cv2.circle(img, (xCalque1,yCalque1),5,(colorValue1,colorValue2,colorValue3),1)
+        cv2.putText(img,'1',(xCalque1-8,yCalque1-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
+        cv2.circle(img, (xCalque2,yCalque2),5,(colorValue1,colorValue2,colorValue3),1)
+        cv2.putText(img,'2',(xCalque2-8,yCalque2-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
+        cv2.circle(img, (xCalque3,yCalque3),5,(colorValue1,colorValue2,colorValue3),1)
+        cv2.putText(img,'3',(xCalque3-8,yCalque3-20),cv2.FONT_HERSHEY_TRIPLEX, 1, (colorValue1,colorValue2,colorValue3))
 
 
-    ##### AFFICHAGE DE LA FRAME APRES AJOUT DES ANNOTATIONS
-    cv2.imshow('frame',img)
+        ##### KEYBOARD LISTENER
+        #On ecoute les touches appuyees
+        key = cv2.waitKey(1) & 0xFF
+        if key==ord("0") or key==ord(" "):
+            currentCalque=0
+        elif key==ord("1"):
+            currentCalque=1
+        elif key==ord("2"):
+            currentCalque=2
+        elif key==ord("3"):
+            currentCalque=3
 
-    ##### DECLENCHAGE DE LA SORTIE DE LA BOUCLE
-    #On quitte quand on appuye sur Echap
-    if key==27:
-        toBreak=True
+        #LEFT
+        elif key==ord('q'):
+            moveCalqueLeft()
+
+        #UP
+        elif key==ord('z'):
+            moveCalqueUp()
+
+        #RIGHT
+        elif key==ord('d'):
+            moveCalqueRight()
+
+        #DOWN
+        elif key==ord('s'):
+            moveCalqueDown()
+
+
+        ##### AFFICHAGE DE LA FRAME APRES AJOUT DES ANNOTATIONS
+        cv2.imshow('frame',img)
+
+        ##### DECLENCHAGE DE LA SORTIE DE LA BOUCLE
+        #On quitte quand on appuye sur Echap
+        if key==27:
+            toBreak=True
 
 ##### LIBERATION DES RESSOURCES
 #On libère la camera
