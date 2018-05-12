a2cdc99a8bc152157e8650aef55aa69637eacfa7
Sam <samuel.rac@telecom-sudparis.eu>
2018-05-12 12:04:31 +0200

    offset match 42

--- src/enums/TasCubes.java
+++ src/enums/TasCubes.java
@@ -8,13 +8,13 @@ public enum TasCubes{
     //Position orange décalée de +15 en Y par rapport à la position théorique
     TAS_BASE(0,new Vec2(640,540), new Vec2(665,555), //MatchScript 0
             new Vec2(645,555), new Vec2(645,555), //MatchScript 2
-            new Vec2(625,540), new Vec2(655,550)), //MatchScript 42
+            new Vec2(630,550), new Vec2(655,550)), //MatchScript 42
 
 
     //Position orange décalée de +10 en Y par rapport à la position théorique
     TAS_CHATEAU_EAU(1,new Vec2(1200,1200),  new Vec2(1195,1215),  //MatchScript 0
             new Vec2(1200,1200),  new Vec2(1200,1200), //MatchScript 2
-            new Vec2(1210,1210),  new Vec2(1190,1220)), //MatchScript 42
+            new Vec2(1190,1190),  new Vec2(1190,1220)), //MatchScript 42
 
 
     //Position verte décalée de -10 en X par rapport à la position théorique
