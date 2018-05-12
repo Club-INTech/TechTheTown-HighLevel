e226bb215387465800ad8d2e3b71082c95cde81c
Sam <samuel.rac@telecom-sudparis.eu>
2018-05-12 06:21:59 +0200

    offset tk 0

--- src/enums/TasCubes.java
+++ src/enums/TasCubes.java
@@ -6,7 +6,7 @@ public enum TasCubes{
     //Position verte décalée de -10 en X par rapport à la position théorique
     //Position orange décalée de +5 en X par rapport à la position théorique
     //Position orange décalée de +15 en Y par rapport à la position théorique
-    TAS_BASE(0,new Vec2(630,540), new Vec2(665,550), //MatchScript 0
+    TAS_BASE(0,new Vec2(640,540), new Vec2(665,550), //MatchScript 0
             new Vec2(645,555), new Vec2(655,550), //MatchScript 2
             new Vec2(630,540), new Vec2(655,550)), //MatchScript 42
 
