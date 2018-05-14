43c2974330ce8cffe245998da70b24defc19cfb1
Nayht <william.huver@teleco-sudparis.eu>
2018-05-14 18:12:54 +0200

    Ajout de la médiane sur un rectangle et de la moyenne sur un rectangle

--- src/imageAnalysis/Picture.java
+++ src/imageAnalysis/Picture.java
@@ -2,7 +2,10 @@ package imageAnalysis;
 
 
 import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public abstract class Picture<T> {
 
@@ -26,13 +29,6 @@ public abstract class Picture<T> {
     public T[][][] getImageArray() {
         return imgArray;
     }
-
-    //////////////////// Image analysis ////////////////////
-
-    public T[] medianOverRectangle(int xStart, int yStart, int width, int height, boolean canGoOutOfBounds){
-        return this.imgArray[xStart][yStart];
-    }
-
 
     /**
      * Set une image RGB
@@ -145,6 +141,110 @@ public abstract class Picture<T> {
         }
         else {
             System.out.println("Bad image type: not RGB or BGR");
+        }
+    }
+
+
+
+    //////////////////// Image analysis ////////////////////
+
+    public T[] medianOverRectangle(int xStart, int yStart, int width, int height, boolean canGoOutOfBounds){
+        if (xStart<this.width && yStart<this.height && xStart>=0 && yStart>=0 && width>0 && height>0){
+            if (canGoOutOfBounds) {
+                width = Math.min(this.width - xStart - 1, width);
+                height = Math.min(this.height - yStart - 1, height);
+                xStart = Math.max(xStart, 0);
+                yStart = Math.max(yStart, 0);
+            }
+            if (xStart+width<this.width && yStart+height<this.height) {
+                List<T> firstParamList = new ArrayList<T>();
+                List<T> secondParamList = new ArrayList<T>();
+                List<T> thirdParamList = new ArrayList<T>();
+                for (int x=xStart; x<xStart+width; x++){
+                    for (int y=yStart; y<yStart+height; y++){
+                        firstParamList.add(this.imgArray[x][y][0]);
+                        secondParamList.add(this.imgArray[x][y][1]);
+                        thirdParamList.add(this.imgArray[x][y][2]);
+                    }
+                }
+                T firstMedian = firstParamList.get(firstParamList.size()/2);
+                T secondMedian = secondParamList.get(secondParamList.size()/2);
+                T thirdMedian = thirdParamList.get(thirdParamList.size()/2);
+                return (T[])new Object[]{firstMedian,secondMedian,thirdMedian};
+            }
+            else {
+                System.out.println("Out of bounds");
+                return null;
+            }
+        }
+        else{
+            System.out.println("Bad parameters: xStart("+xStart+") yStart("+yStart+") width("+width+") height("+height+")"+
+                    " canGoOutOfBounds("+canGoOutOfBounds+") imageWidth("+this.width+") imageHeight"+this.height+")");
+            return null;
+        }
+    }
+
+    public T[] averageOverRectangle(int xStart, int yStart, int width, int height, boolean canGoOutOfBounds){
+        if (xStart<this.width && yStart<this.height && xStart>=0 && yStart>=0 && width>0 && height>0){
+            if (canGoOutOfBounds) {
+                width = Math.min(this.width - xStart - 1, width);
+                height = Math.min(this.height - yStart - 1, height);
+                xStart = Math.max(xStart, 0);
+                yStart = Math.max(yStart, 0);
+            }
+            if (xStart+width<this.width && yStart+height<this.height){
+                int nbPixelsChecked=0;
+                if (this.imgArray instanceof Float[][][]) {
+                    float firstParamSum = 0;
+                    float secondParamSum = 0;
+                    float thirdParamSum = 0;
+                    for (int x = xStart; x < xStart + width; x++) {
+                        for (int y = yStart; y < yStart + height; y++) {
+                            nbPixelsChecked++;
+                            firstParamSum += (float) this.imgArray[x][y][0];
+                            secondParamSum += (float)this.imgArray[x][y][1];
+                            thirdParamSum += (float) this.imgArray[x][y][2];
+                        }
+                    }
+                    float firstParamAverage=firstParamSum/nbPixelsChecked;
+                    float secondParamAverage=secondParamSum/nbPixelsChecked;
+                    float thirdParamAverage=thirdParamSum/nbPixelsChecked;
+                    return (T[])new Object[]{firstParamAverage, secondParamAverage, thirdParamAverage};
+                }
+                else if (this.imgArray instanceof Integer[][][]){
+                    int firstParamSum = 0;
+                    int secondParamSum = 0;
+                    int thirdParamSum = 0;
+                    for (int x = xStart; x < xStart + width; x++) {
+                        for (int y = yStart; y < yStart + height; y++) {
+                            nbPixelsChecked++;
+                            firstParamSum += (int) this.imgArray[x][y][0];
+                            secondParamSum += (int)this.imgArray[x][y][1];
+                            thirdParamSum += (int) this.imgArray[x][y][2];
+                        }
+                    }
+                    int firstParamAverage=Math.round(firstParamSum*1f/nbPixelsChecked);
+                    int secondParamAverage=Math.round(secondParamSum*1f/nbPixelsChecked);
+                    int thirdParamAverage=Math.round(thirdParamSum*1f/nbPixelsChecked);
+                    return (T[])new Object[]{firstParamAverage,secondParamAverage,thirdParamAverage};
+                }
+                else{
+                    System.out.println("Ce cas ne devrait pas arriver");
+                    System.out.println("firstParamSum = 0");
+                    System.out.println("secondParamSum = 0");
+                    System.out.println("thirdParamSum = 0");
+                    return (T[])new Object[]{0,0,0};
+                }
+            }
+            else {
+                System.out.println("Out of bounds");
+                return null;
+            }
+        }
+        else{
+            System.out.println("Bad parameters: xStart("+xStart+") yStart("+yStart+") width("+width+") height("+height+")"+
+                    " canGoOutOfBounds("+canGoOutOfBounds+") imageWidth("+this.width+") imageHeight"+this.height+")");
+            return null;
         }
     }
 }
